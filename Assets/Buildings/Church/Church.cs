6edd90ea49fb9d779d3617b44895e23f8c8c5022
Jan Kubica <kubica.j88@gmail.com>
2025-01-19 17:08:44 +0100

    basic church choice added

--- Assets/Buildings/Church/Church.cs
+++ Assets/Buildings/Church/Church.cs
@@ -1,12 +1,14 @@
 using System.Collections;
+using TMPro;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Church : BuildingAbstract
 {
+    [SerializeField] private ChurchPopUp popUp;
 
     public override void Use()
     {
-        Debug.Log("assssssssssssss");
+        popUp.gameObject.SetActive(true);
     }
 }
---
+++ Assets/Buildings/Church/ChurchPopUp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChurchPopUp : MonoBehaviour
+{
+    public Button leftChoice;
+    public Button rightChoice;
+}

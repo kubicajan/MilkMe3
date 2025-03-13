ab539d46d4280a4add0ba6e98188d5097ec66068
janku <kubica.j88@entity.cz>
2025-03-13 21:44:28 +0100

    Boss heavy attack now hurts

--- Assets/Living/Enemies/WarriorBoss.cs
+++ Assets/Living/Enemies/WarriorBoss.cs
@@ -1,7 +1,36 @@
+using UnityEngine;
+
 namespace Living.Enemies
 {
 	public class WarriorBoss : EnemyScript
 	{
+		[SerializeField] private Transform attackPoint;
+		public LayerMask enemyLayers;
 
+		private const float MEELE_ATTACK_RANGE = 3f;
+
+		public void MeeleAttack()
+		{
+			const float KNOCKBACK = 2;
+			DealDamageTo(DetectEnemiesInRange(MEELE_ATTACK_RANGE), KNOCKBACK);
+		}
+
+		private void DealDamageTo(Collider2D[] detectedEnemies, float knockBack)
+		{
+			foreach (Collider2D enemy in detectedEnemies)
+			{
+				if (enemy.TryGetComponent<LivingEntity>(out var enemyScript))
+				{
+					enemyScript.TakeDamage(10);
+					enemyScript.GetKnockedBack(this.transform.position, knockBack);
+					Debug.Log("hit something");
+				}
+			}
+		}
+
+		private Collider2D[] DetectEnemiesInRange(float range)
+		{
+			return Utility.DetectByLayers(attackPoint.position, range, enemyLayers);
+		}
 	}
 }

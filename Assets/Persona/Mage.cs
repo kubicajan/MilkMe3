e9291c7ea5847dfdb509a65cadb42917c4423d9c
kubicajan <kubica.j88@gmail.com>
2025-06-30 22:12:07 +0200

    tornado added to mage

--- Assets/Persona/Mage.cs
+++ Assets/Persona/Mage.cs
@@ -3,60 +3,67 @@ using UnityEngine;
 
 namespace Persona
 {
-    public class Mage : PersonaAbstract
-    {
-        [SerializeField] private ParticleSystem shieldParticleEffect;
+	public class Mage : PersonaAbstract
+	{
+		[SerializeField] private ParticleSystem shieldParticleEffect;
+		[SerializeField] private GameObject tornadoPrefab;
 
-        public override string PersonaName { get; set; } = "Mage";
+		public override string PersonaName { get; set; } = "Mage";
 
-        public override void BaseAttack()
-        {
-            Debug.Log("Unfinished");
-            return;
-        }
+		public override void BaseAttack()
+		{
+			Debug.Log("Unfinished");
+			return;
+		}
 
-        public override void FirstAbility()
-        {
-            Debug.Log("Unfinished");
-            return;
-        }
+		public override void FirstAbility()
+		{
+			ShootTornado();
+		}
 
-        public override void SecondAbility()
-        {
-            MagicPushBack();
-        }
+		public override void SecondAbility()
+		{
+			MagicPushBack();
+		}
 
-        public override void SwapToMe()
-        {
-            ActivateShield();
-        }
+		public override void SwapToMe()
+		{
+			ActivateShield();
+		}
 
-        public override void SwapFromMe()
-        {
-            DeactivateShield();
-        }
+		public override void SwapFromMe()
+		{
+			DeactivateShield();
+		}
 
-        private void ActivateShield()
-        {
-            shieldParticleEffect.Play();
-            shieldParticleEffect.GetComponent<CircleCollider2D>().enabled = true;
-        }
+		private void ShootTornado()
+		{
+			Instantiate(tornadoPrefab, transform.position, gameObject.transform.rotation);
+		}
 
-        private void DeactivateShield()
-        {
-            shieldParticleEffect.Stop();
-            shieldParticleEffect.GetComponent<CircleCollider2D>().enabled = false;
-        }
+		private void ActivateShield()
+		{
+			shieldParticleEffect.Play();
+			shieldParticleEffect.GetComponent<CircleCollider2D>().enabled = true;
+		}
 
-        private void MagicPushBack()
-        {
-            const float DISTANCE_LIMIT = 10f;
-            MagicPushAllEnemies(DetectEnemiesInRange(DISTANCE_LIMIT / 2), transform.position, DISTANCE_LIMIT);
-        }
+		private void DeactivateShield()
+		{
+			shieldParticleEffect.Stop();
+			shieldParticleEffect.GetComponent<CircleCollider2D>().enabled = false;
+		}
 
-        private void MagicPushAllEnemies(Collider2D[] detectedEntities, Vector2 perpetratorPosition, float distanceLimit)
-        {
-            ProcessEnemies(detectedEntities, enemyScript => enemyScript.MagicPushMe(perpetratorPosition, distanceLimit));
-        }
-    }
+		private void MagicPushBack()
+		{
+			const float DISTANCE_LIMIT = 10f;
+			MagicPushAllEnemies(DetectEnemiesInRange(DISTANCE_LIMIT / 2), transform.position, DISTANCE_LIMIT);
+		}
+
+		private void MagicPushAllEnemies(Collider2D[] detectedEntities, Vector2 perpetratorPosition,
+			float distanceLimit)
+		{
+			ProcessEnemies(detectedEntities,
+				enemyScript => enemyScript.MagicPushMe(perpetratorPosition, distanceLimit));
+		}
+	}
 }

96ca0382b5552ee32088b13ec34b5d9ecd2123b0
honza <kubica.j88@gmail.com>
2026-01-04 19:07:34 +0100

    moonwalking chicken added

---
+++ Assets/Code/Living/NPCs/ChickenNpcScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Code.Living.NPCs
+{
+	public class ChickenNpcScript : NpcScript
+	{
+		[SerializeField] private Rigidbody2D rb;
+		private float moveSpeed = 2f;
+		private float changeDirectionTime = 2f;
+
+		private float timer;
+		private int direction;
+
+		private void Update()
+		{
+			timer -= Time.deltaTime;
+			if (timer <= 0)
+			{
+				ChooseDirection();
+			}
+		}
+
+		private void FixedUpdate()
+		{
+			rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+		}
+
+		private void ChooseDirection()
+		{
+			direction = Random.Range(0, 2) == 0 ? -1 : 1;
+			timer = changeDirectionTime;
+			FlipSprite();
+		}
+
+		private void FlipSprite()
+		{
+			transform.localScale = new Vector3(direction, 1, 1);
+		}
+	}
+}

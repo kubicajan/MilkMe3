7333897bde9a7e2b0045d3ee0e170231321d9a13
honza <kubica.j88@gmail.com>
2026-01-04 21:12:55 +0100

    NPCs inherit from common class

--- Assets/Code/Living/NPCs/NpcScript.cs
+++ Assets/Code/Living/NPCs/NpcScript.cs
@@ -1,14 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcScript : MonoBehaviour
+namespace Code.Living.NPCs
 {
-    public ParticleSystem kam;
-
-    public void DoDialog()
+    public class NpcScript : MonoBehaviour
     {
-        DialogManager.Instance.PopUpDialog("AMBATAKAM", gameObject.transform.position);
-        kam.Play();
+        public ParticleSystem kam;
+
+        public void DoDialog()
+        {
+            DialogManager.Instance.PopUpDialog("AMBATAKAM", gameObject.transform.position);
+            kam.Play();
+        }
+
+        [SerializeField] private Rigidbody2D rb;
+        private float moveSpeed = 2f;
+        private float changeDirectionTime = 2f;
+
+        private float timer;
+        private int direction;
+
+        private void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                ChooseDirection();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+        }
+
+        private void ChooseDirection()
+        {
+            direction = Random.Range(0, 2) == 0 ? -1 : 1;
+            timer = changeDirectionTime;
+            FlipSprite();
+        }
+
+        private void FlipSprite()
+        {
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
     }
 }
--- Assets/Code/Living/NPCs/RabbitNpcScript.cs
+++ Assets/Code/Living/NPCs/RabbitNpcScript.cs
@@ -1,41 +1,6 @@
-using UnityEngine;
-using Random = UnityEngine.Random;
-
 namespace Code.Living.NPCs
 {
 	public class RabbitNpcScript : NpcScript
 	{
-		[SerializeField] private Rigidbody2D rb;
-		private float moveSpeed = 2f;
-		private float changeDirectionTime = 2f;
-
-		private float timer;
-		private int direction;
-
-		private void Update()
-		{
-			timer -= Time.deltaTime;
-			if (timer <= 0)
-			{
-				ChooseDirection();
-			}
-		}
-
-		private void FixedUpdate()
-		{
-			rb.linearVelocity = new Vector2(-direction * moveSpeed, rb.linearVelocity.y);
-		}
-
-		private void ChooseDirection()
-		{
-			direction = Random.Range(0, 2) == 0 ? -1 : 1;
-			timer = changeDirectionTime;
-			FlipSprite();
-		}
-
-		private void FlipSprite()
-		{
-			transform.localScale = new Vector3(direction, 1, 1);
-		}
 	}
 }
